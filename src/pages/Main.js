96e55c8f3999c9f7dfb61322f19523fdd1afea41
Lucas Zago <lcardosozago@gmail.com>
2019-08-10 05:16:09 -0400

    MATCH

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
 import AsyncStorage from "@react-native-community/async-storage";
 import {
   View,
@@ -14,10 +15,12 @@ import api from "../services/api";
 import logo from "../assets/logo.png";
 import like from "../assets/like.png";
 import dislike from "../assets/dislike.png";
+import itsamatch from "../assets/itsamatch.png";
 
 export default function Main({ navigation }) {
   const id = navigation.getParam("user");
   const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
   useEffect(() => {
     async function loadUsers() {
@@ -31,6 +34,16 @@ export default function Main({ navigation }) {
     }
 
     loadUsers();
+  }, [id]);
+
+  useEffect(() => {
+    const socket = io("http://localhost:3333", {
+      query: { user: id }
+    });
+
+    socket.on("match", dev => {
+      setMatchDev(dev);
+    });
   }, [id]);
 
   async function handleLike() {
@@ -110,6 +123,25 @@ export default function Main({ navigation }) {
           </View>
         )}
       </View>
+
+      {matchDev && (
+        <View style={styles.matchContainer}>
+          <Image style={styles.matchImage} source={itsamatch} />
+          <Image
+            style={styles.matchAvatar}
+            source={{
+              uri: matchDev.avatar
+            }}
+          />
+
+          <Text style={styles.matchName}>{matchDev.name}</Text>
+          <Text style={styles.matchBio}>{matchDev.bio}</Text>
+
+          <TouchableOpacity onPress={() => setMatchDev(null)}>
+            <Text style={styles.closeMatch}>FECHAR</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </SafeAreaView>
   );
 }
@@ -188,5 +220,44 @@ const styles = StyleSheet.create({
       width: 0,
       height: 2
     }
-  }
+  },
+  matchContainer: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 9999,
+  },
+  matchImage: {
+    height: 60,
+    resizeMode: 'contain',
+  },
+  matchAvatar: {
+    width: 160,
+    height: 160,
+    borderRadius: 80,
+    borderWidth: 5,
+    borderColor: '#fff',
+    marginVertical: 30
+  },
+  matchName: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  matchBio: {
+    marginTop: 10,
+    fontSize: 16,
+    color: 'rgba(255, 255, 255, 0.8)',
+    lineHeight: 24,
+    textAlign: 'center',
+    paddingHorizontal: 30,
+  },
+  closeMatch: {
+    fontSize: 16,
+    color: 'rgba(255, 255, 255, 0.8)',
+    textAlign: 'center',
+    marginTop: 30,
+    fontWeight: 'bold',
+  },
 });
